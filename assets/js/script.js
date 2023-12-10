19bbd13c91280005f6b89c61c1fc8440e31252d0
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2023-12-10 15:35:21 -0500

    added getTime and update the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // Global element variable declaration
+  headerDateEl = $("#currentDay");
+  
+  // Get the current time using the DayJS API
+  function getTime () {
+    now = dayjs()
+    return now;
+  }
+  
+  // Updates the current time in the header
+  function headerTimeUpdate() {
+    var rightNow = getTime();
+    headerDateEl.text(rightNow.format("dddd, MMMM DD"));
+  }  
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +35,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  setInterval(headerTimeUpdate, 1000);
 });
